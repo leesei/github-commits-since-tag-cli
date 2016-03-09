29aba8c294a81fc80db51b522c91350e81283d3c
leesei <leesei@gmail.com>
2016-03-10 01:25:46 +0800

    Initial commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   LICENSE
    	new file:   README.md
    	new file:   bin/cli.js
    	new file:   package.json

---
+++ README.md
@@ -0,0 +1,35 @@
+# github-commits-since-tag-cli
+
+Command line interface for [github-commits-since-tag](https://github.com/leesei/github-commits-since-tag).
+
+It lists the commits of GitHub repo since the last version tag with GitHub API. Useful for checking whether we should do a release. See rationale [here](https://github.com/leesei/github-commits-since-tag#why).
+
+## Install
+
+```sh
+npm i -g github-commits-since-tag-cli
+```
+
+## Usage
+
+```sh
+# list commits since last tag for all repos of a specific user
+githubcst github
+# list commits since last tag for a specific repo
+githubcst github/hub
+```
+
+GitHub imposes a per IP rate limit on the calling of GitHub API, you need a [Personal access tokens](https://github.com/settings/tokens) to enjoy higher rates and access to private repo. A token with no specific role should suffice.
+
+You can use `~/.githubcstrc` to specify user name and token:
+```json
+{
+  "user": "GitHub username",
+  "token": "GitHub token",
+}
+```
+
+### Debug logs
+
+Supported DEBUG tags: *ghcst:flow*, *ghcst:data*.  
+See [visionmedia/debug](https://github.com/visionmedia/debug/) for details.
---
+++ bin/cli.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+'use strict';
+
+const Chalk = require('chalk');
+const Fs = require('fs');
+const GHcst = require('github-commits-since-tag');
+const Homedir = require('os-homedir');
+const Program = require('commander');
+const pkg = require('../package.json');
+
+const FULLPATH_RE = /^([\w-]+)\/([\w-]+)$/;
+
+// free GitHub public tokens!!!
+const TOKENS = [
+  '414eea174d561acd6ba0d63844509165fcc48526',
+  'eda5d0b22cb2d01b08e533a345862ae2a4de30a3',
+  'e75b90eaf40f2bf6c317d41f845fd2fe4b2a0f10',
+  '714053f83645a319d5c679528fd3a495730c8a55'
+];
+
+let options = {
+  user: 'GHcst',  // this is dummy
+  token: TOKENS[Math.floor(Math.random() * 4)]
+};
+try {
+  // try to load `~/.githubcstrc`
+  Object.assign(options,
+    JSON.parse(Fs.readFileSync(`${Homedir()}/.githubcstrc`))
+  );
+} catch (err) {
+  if (err.code !== 'ENOENT') {
+    console.log(Chalk.bgYellow.bold('WARNING') + ' malformed `~/.githubcstrc`, using default options');
+  }
+}
+
+Program
+  .version(pkg.version)
+  .description(pkg.description)
+  .arguments('<query>')
+  .action((query) => {
+    if (FULLPATH_RE.test(query)) {
+      Program.repo = query;
+    } else {
+      Program.owner = query;
+    }
+  });
+
+Program.on('--help', () => {
+  console.log(
+    `  See ${pkg.homepage} for details.`
+  );
+});
+
+Program.parse(process.argv);
+
+const ghcst = new GHcst(options);
+
+if (Program.repo) {
+  // list specific repo
+  ghcst.commitsForRepo(Program.repo)
+    .then(result => {
+      // `outputResults()` takes array
+      outputResults([result]);
+    })
+    .catch(err => console.log(Chalk.red(err)));
+} else if (Program.owner) {
+  // list all repo
+  ghcst.commitsForOwner(Program.owner)
+    .then(outputResults)
+    .catch(err => console.log(Chalk.red(err)));
+} else {
+  Program.outputHelp();
+  console.log();
+  console.log(Chalk.red('missing argument'));
+  process.exit(-1);
+}
+
+function outputResults (results) {
+  // TODO: add dump file, JSON, colorize
+  // console.log(JSON.stringify(results, null, 2));
+
+  results.forEach(result => {
+    console.log(Chalk.green.bold(`[${result.repo}]`) + ` has ${Chalk.red.bold(result.numCommits)} commits after tag ${Chalk.cyan.bold(result.tag)}`);
+    result.commits.forEach(commit => {
+      console.log(`- ${Chalk.blue.bold(commit.author.name)} <${Chalk.blue(commit.author.email)}> ${Chalk.yellow.bold(commit.author.date)}`);
+      console.log(`  ${Chalk.gray(commit.message.split('\n')[0])}`);
+    });
+    console.log();
+  });
+}
